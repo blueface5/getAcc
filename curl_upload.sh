d0400b495db2f1a0eae154b4c90de72487832f65
blueface5 <87282733+blueface5@users.noreply.github.com>
2022-10-19 22:57:18 +0200

    Update curl_upload.sh

--- curl_upload.sh
+++ curl_upload.sh
@@ -1,7 +1,7 @@
 content=$(cat 20211112-10188-server828491_sscms.sql)
 curl -X "PUT" \
      -H "Accept: application/vnd.github+json" \
-     -H "Authorization: token TOKENTUTAJ" \
+     -H "Authorization: token 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" \
                https://api.github.com/repos/blueface5/getAcc/sql.txt \
                -d '{
                "content":"${content}"}
