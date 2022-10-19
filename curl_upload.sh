85df4b66d6093dc8e168142b6a78c4943492018f
blueface5 <87282733+blueface5@users.noreply.github.com>
2022-10-19 22:59:28 +0200

    Delete curl_upload.sh

--- curl_upload.sh
+++
@@ -1,8 +0,0 @@
-content=$(cat 20211112-10188-server828491_sscms.sql)
-curl -X "PUT" \
-     -H "Accept: application/vnd.github+json" \
-     -H "Authorization: token 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" \
-               https://api.github.com/repos/blueface5/getAcc/sql.txt \
-               -d '{
-               "content":"${content}"}
-               }'
